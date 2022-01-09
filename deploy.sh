65f267e03bb68020bb34f66262195789de50bea4
Phyroks <52051341+Phyroks@users.noreply.github.com>
2022-01-09 17:03:40 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 echo Download Theme CSS!
 
 cd /var/www/html/admin/style/themes
-curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/discord.css --output discord-theme.css
+curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/discord.css > discord-theme.css
 sleep 5
 echo updating themeswitcher!
 
 cd /var/www/html/admin/scripts/pi-hole/php
-curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/theme.php --output theme.php
+curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/theme.php > theme.php
 
 sleep 5
 echo Installation finished
