958dfea973691ee5cb6d263993c7d284430e20c5
Phyroks <52051341+Phyroks@users.noreply.github.com>
2022-01-09 16:40:49 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cd /var/www/html/admin/style/themes
+curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/discord.css --output discord-theme.css
+
+
+cd /var/www/html/admin/scripts/pi-hole/php
+curl https://github.com/Phyroks/pihole-theme/raw/main/theme.php --output theme.php
+
+
