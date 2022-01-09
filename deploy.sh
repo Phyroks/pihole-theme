0cf4fdafd8db180aa11784be464d44d654cdbdef
Phyroks <52051341+Phyroks@users.noreply.github.com>
2022-01-09 16:46:07 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/discord.css --o
 
 
 cd /var/www/html/admin/scripts/pi-hole/php
-curl https://github.com/Phyroks/pihole-theme/raw/main/theme.php --output theme.php
+curl https://raw.githubusercontent.com/Phyroks/pihole-theme/main/theme.php --output theme.php
 
 
